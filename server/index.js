ea6522dbb06a831da4a62519c6aecff73acdba0f
Tim Kang <tikang.cs@gmail.com>
2021-03-25 15:21:46 -0400

    TKs version

--- server/index.js
+++ server/index.js
@@ -5,13 +5,16 @@ const path = require('path');
 const axios = require('axios');
 const TOKEN = require('../config.js');
 const compression = require('compression');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.use(express.json());
 app.use(compression());
 app.use(express.static('dist'));
 
 app.get('/*', function (req, res, next) {
-  const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc${req.url}`;
+  const url = `https://${process.env.EC2_URL}`; //TODO replace with process.env that updates to the EC2 Instance URL
   axios.get(url, {
     headers: {
       'AUTHORIZATION': TOKEN,
